d09e414074e8cd639cea6290c91df7f7d3c4b7f9
Jacky Wang <jacky0105wang@gmail.com>
2017-08-12 08:38:09 +1000

    Initial Commit

---
+++ 5140week3_inventory_coredataTests/_140week3_inventory_coredataTests.swift
@@ -0,0 +1,36 @@
+//
+//  _140week3_inventory_coredataTests.swift
+//  5140week3_inventory_coredataTests
+//
+//  Created by Jiaqi Wang on 12/8/17.
+//  Copyright © 2017 Jiaqi Wang. All rights reserved.
+//
+
+import XCTest
+@testable import _140week3_inventory_coredata
+
+class _140week3_inventory_coredataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
